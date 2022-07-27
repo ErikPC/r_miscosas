de9ff5b63b416533437e4c0d93d7943cb16d77b3
ErikPC <epardillo@cifpfbmoll.eu>
2022-07-28 01:36:48 +0200

    feat: añadir pedir precio por consola

--- reglaTres.r
+++ reglaTres.r
@@ -1,8 +1,12 @@
 yen <- 8676
 eur <- 61.76
 
-item <- 7584
-delivery <- 6184
+print("Write price ¥ of item with out ',' or '.' ")
+item <- readline()
+print("Write price ¥ of delivery with out ',' or '.' ")
+delivery <- readline()
+item <- as.numeric(item)
+delivery <- as.numeric(delivery)
 
 
 price <- ((item + delivery) * eur) / yen
